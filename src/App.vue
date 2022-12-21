16ed430983600814462fcf2f919ca7f1305403ce
Fadeta <82428061+Fadeta@users.noreply.github.com>
2022-12-21 14:19:29 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,112 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default{
+  data(){
+    return{
+      nama: ""
+    }
+
+  },
+  methods:{
+    input(){
+      this.nama = "fadeta"
+    },
+    async ambildata() {
+      const response = await fetch("http://localhost:3000/suhu1")
+      const data = await response.json();
+      this.nama = data.SUHU
+      console.log(data)
+    },
+  },
+}
+
+
+</script>
+
+<template>
+  <header>
+    <input type="text" v-model="nama"/>
+    {{ nama }}
+    <button @click="input()">Input</button>
+    <button @click="ambildata()">Ambil Data</button>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="300" height="300" />
+
+    <div class="wrapper">
+      <HelloWorld msg="Fadeta Ilhan Gandhi" />
+
+      <!-- <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav> -->
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+  
+}
+
+.logo {
+  display: block;
+  margin: 1 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: blueviolet;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
